8925482a05c7a1fddd157157b1cc6598cc141bb6
Gunjan Rathore <rathoregunjan87@gmail.com>
2024-07-17 19:29:29 +0530

    second commit

--- public/src/App.js
+++ public/src/App.js
@@ -5,12 +5,13 @@ import Login from "./pages/Login";
 import Chat from "./pages/Chat";
 
 export default function App() {
-  return <BrowserRouter>
+  return (<BrowserRouter>
     <Routes>
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/" element={<Chat />} />
     </Routes>
-  </BrowserRouter>;
+  </BrowserRouter>
+  );
 }
 
